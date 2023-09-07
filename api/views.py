6a576108a1433a0914581fb17c18855b417f2bc1
belemaaa <graceitamunoala@gmail.com>
2023-09-07 18:15:33 +0000

    create-employee

--- api/views.py
+++ api/views.py
@@ -7,6 +7,7 @@ from . import models
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated
 
 
 class AdminSignup(APIView):
@@ -44,10 +45,32 @@ class AdminLogin(APIView):
             except models.Admin.DoesNotExist:
                 user = None
             if user is not None and check_password(password, user.password):
-                access_token = Token.objects.get_or_create(user=user)
-                return Response({'message': 'login successful', 'access_token': access_token.key}, status=status.HTTP_200_OK)
+                token, created = Token.objects.get_or_create(user=user)
+                return Response({'message': 'login successful',
+                                 'access_token': token.key}, status=status.HTTP_200_OK) 
             return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 class CreateEmployee(APIView):
-    pass
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        serializer = serializers.EmployeeSerializer(data=request.data)
+
+        if serializer.is_valid():
+            first_name = serializer.validated_data.get('first_name')
+            last_name = serializer.validated_data.get('last_name')
+            email = serializer.validated_data.get('email')
+            phone_number = serializer.validated_data.get('phone_number')
+            employee_id = serializer.validated_data.get('employee_id')
+            address = serializer.validated_data.get('address')
+            position = serializer.validated_data.get('position')
+            sex = serializer.validated_data.get('sex')
+            state_of_origin = serializer.validated_data.get('state_of_origin')
+            date_of_birth = serializer.validated_data.get('date_of_birth')
+
+            serializer.save(user=self.request.user)
+            return Response({'message': 'Employee creation was successful',
+                             'data': serializer.data}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
